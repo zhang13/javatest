ac0f3bb31e39a4c7f542f71bb3c0be1480a720a9
charles.zhang13 <290834891@qq.com>
2017-06-13 10:07:58 +0800

    hutool

---
+++ src/com/hutools/UtilTest.java
@@ -0,0 +1,13 @@
+package com.hutools;
+
+import com.xiaoleilu.hutool.util.PageUtil;
+
+public class UtilTest {
+
+	public static void main(String[] args) {
+
+		System.out.println(PageUtil.totalPage(100, 30));
+		
+	}
+
+}
